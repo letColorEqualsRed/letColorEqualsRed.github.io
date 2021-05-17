5d7b7c695be3bbf3928860b23655ddd397464724
Isaac Tsui <isaac.tsui.343@gmail.com>
2021-05-17 22:40:57 +0800

    Adding build folder

---
+++ build/precache-manifest.78b33f767205a78c842dadc64c0e414d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d54d6cb0d1c8de1d10113716d19a1b",
+    "url": "/visual-novel-composer/index.html"
+  },
+  {
+    "revision": "6fa184af45b9a5cdce05",
+    "url": "/visual-novel-composer/static/css/main.42fe460b.chunk.css"
+  },
+  {
+    "revision": "524508b5314c41927d07",
+    "url": "/visual-novel-composer/static/js/2.f353edc7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/visual-novel-composer/static/js/2.f353edc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02a2bfede05ef490d57",
+    "url": "/visual-novel-composer/static/js/3.d07e248d.chunk.js"
+  },
+  {
+    "revision": "6fa184af45b9a5cdce05",
+    "url": "/visual-novel-composer/static/js/main.11660d75.chunk.js"
+  },
+  {
+    "revision": "1537fae31788ce216514",
+    "url": "/visual-novel-composer/static/js/runtime-main.a6b66b2c.js"
+  }
+]);
